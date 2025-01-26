b710a9f0e6869f2da51f19d8b482ca4b453a358c
Bryan Chen <abryanchen@hotmail.fr>
2025-01-26 14:13:33 +0100

    try to patch to mobile the stuck button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function moveRandomEl(elm) {
   elm.style.position = "absolute";
-  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+  elm.style.top = Math.floor(Math.random() * 80 + 10) + "%";
+  elm.style.left = Math.floor(Math.random() * 80 + 10) + "%";
 }
 
 const moveRandom = document.querySelector("#move-random");
